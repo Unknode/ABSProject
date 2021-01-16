2f769b77ad15042a6ce9687e48e8196e7c1809cd
Unknode <carlosgabriel.m99@hotmail.com>
2021-01-15 23:29:30 -0300

    Projeto testado

---
+++ ABSProject/ABSProject/Entities/Drink.cs
@@ -0,0 +1,48 @@
+﻿using ABSProject.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABSProject.Entities {
+    class Drink {
+
+        public string Type { get; set; }
+        public Enum Flavor { get; set; }
+        public Enum CupType { get; set; }
+        public int IceCount { get; set; }
+        public string Size { get; set; }
+        public string OrderType { get; set; }
+
+        public Drink(string type,Enum flavor,Enum cupType,int iceCount,string size,string orderType) {
+            Type = type;
+            Flavor = flavor;
+            CupType = cupType;
+            IceCount = iceCount;
+            Size = size;
+            OrderType = orderType;
+        }
+
+        public Drink(string type,Enum flavor,Enum cupType,string size,string orderType) {
+            Type = type;
+            Flavor = flavor;
+            CupType = cupType;
+            Size = size;
+            OrderType = orderType;
+
+        }
+
+        public Drink() { }
+
+        
+        public override string ToString() { //Retorna o objeto em formato de string para facilitar o print
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Seu pedido é: ").Append(Type + " ").Append("Sabor: " + Flavor).Append(", Copo: " + CupType).Append(", Cubos de gelo:" + IceCount);
+            sb.Append(", Tamanho: " + Size).Append("ml").Append(", Tipo da Tampa: " + OrderType);
+
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ ABSProject/ABSProject/Entities/Enums/CupType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABSProject.Entities.Enums {
+    enum CupType:int {
+        Papel,
+        Plastico
+    }
+}
