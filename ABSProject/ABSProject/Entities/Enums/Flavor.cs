99cff17421eda55b68209fd4a0ece878cf344cc5
Unknode <carlosgabriel.m99@hotmail.com>
2021-01-15 23:34:16 -0300

    Upando pro git

---
+++ ABSProject/ABSProject/Entities/Enums/Flavor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABSProject.Entities.Enums {
+    enum Flavor: int {
+
+        Coca = 1,
+        Guarana = 2,
+        Uva = 3,
+        Laranja = 4
+    }
+}
